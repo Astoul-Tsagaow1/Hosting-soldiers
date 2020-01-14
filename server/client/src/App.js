63dfc315e890fdef6305daf73a056efeb88700fa
Shay-bicha <shaybicha2@gmail.com>
2020-01-14 11:33:23 +0200

    update project

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,39 +1,35 @@
-import React ,{Component} from 'react';
-import './App.css';
-import axios from 'axios';
-// import {BrowserRouter , Link , Switch , Route} from 'react-router-dom';
-import SignUpSoldiers from './SignUpSoldiers';
-class App extends Component{
-  url = "/api";
-  state = {data : ''};
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-  clickHandler = () =>{
+class App extends Component {
+  url = "/api";
+  state = { data: "" };
+
+  clickHandler = () => {
     console.log("clicked");
-    axios.get("/api")
-    .then(res => {
-      console.log(res.data);
-      this.setState({data : res.data.res});
-  })
-    .catch(err => console.log(err));
-  }
-  render(){
+    axios
+      .get(this.url)
+      .then(res => {
+        console.log(res.data.res);
+        this.setState({ data: res.data.res });
+      })
+      .catch(err => console.log(err));
+    // fetch(this.url)
+    //   .then(res => res.json())
+    //   .then(data => this.setState({data : data.res}))
+    //   .catch(err => console.log(err));
+  };
+
+  render() {
     return (
-      <div>
-        <h2>Hello World</h2>
-        <button onClick = {this.clickHandler}>Click Me</button>
-        <p>{this.state.data}</p>
-        <SignUpSoldiers/>
+      <div className="App">
+        <p>Hello Nathan Krasney</p>
+        <button onClick={this.clickHandler}>Access express server !!!!!</button>
+        <p>Got : {this.state.data}</p>
       </div>
-      // <BrowserRouter>
-      //   <Link to = '/'>Home|</Link>
-      //   <Link to = '/'>Logup Soldiers|</Link>
-      //   <Link to = '/'>Logup Family|</Link>
-      //   <Link to = '/'>Login|</Link>
-      //   <Link to = '/'>About|</Link>
-      // </BrowserRouter> 
     );
   }
- 
 }
 
 export default App;
