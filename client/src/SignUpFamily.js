3b43522d78699a99896c4363a0885111793aef65
Shay-bicha <shaybicha2@gmail.com>
2020-01-16 10:04:49 +0200

    update after separate dr client and dr server

--- client/src/SignUpFamily.js
+++ client/src/SignUpFamily.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './SignUpFamily.css'
-
 export default class SignUpFamily extends Component {
-    state ={}
-
+    state = {}
     constructor(props) {
-
         super(props)
         this.state = {
             fname: "",
@@ -14,68 +11,93 @@ export default class SignUpFamily extends Component {
             fPhonNumber: "",
             fNumberSoldiersHosts: "",
             fPassword: "",
-            ConfirmePassword: "", 
+            ConfirmePassword: "",
             FamilyDescriptionvlue: "",
             fCity: "",
-           
-
         }
         this.Hendelchange = this.Hendelchange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-
-    Hendelchange(e){
+    Hendelchange(e) {
         this.setState({
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         })
     }
-    
-
-
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log("form");
+        console.log(event.target.fname.value);
+        const Familyobj = {
+            familyname: this.state.fname,
+            familyemail: this.state.femail,
+            familyPhonNumber: this.state.fPhonNumber,
+            familyNumberSoldiersHosts: this.state.fNumberSoldiersHosts,
+            familyPassword: this.state.fPassword,
+            familyConfirmePassword: this.state.ConfirmePassword,
+            familyCity: this.state.fCity
+        }
+        console.log(Familyobj);
+        axios.post('/family', { Familyobj })
+            .then(response => {
+                alert("family")
+                console.log(response);
+                console.log(response.data);
+            })
+    }
     render() {
-
         return (
-            <div>
-                <form className="SignUp-Family-htmlForm" onSubmit={this.props.send}>
+            <div className="row">
+                <div className="col-75">
+                    <div className="bg-image"></div>
+                    <div className="container">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="row">
+                                <div className="col-50">
+                                    <h1>Family Form</h1>
+                                    <label htmlFor="fname"> Family-Name :
+                 <input value={this.state.fname} name="fname" type="text" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    <label htmlFor="email">  e-mail  :
+                 <input value={this.state.femail} name="femail" type="email" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    <label htmlFor="Phone-Number">  Phone-Number  :
+                 <input value={this.state.fPhonNumber} name="fPhonNumber" id="Phone-Number" type="number" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    <label htmlFor="City"> City :
+                 <input value={this.state.fCity} name="fCity" id="City" type="text" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                </div>
+                                <div className="col-50">
+                                    <h1>Family Form</h1>
+                                    <label htmlFor="NumberSoldiers">  Number of soliders you can host :
+                   <input value={this.state.fNumberSoldiersHosts} name="fNumberSoldiersHosts" id="NumberSoldiers" type="number" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    <label htmlFor="Password">   Password :
+                   <input value={this.state.fPassword} name="fPassword" type="Password" id="Password" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    <label htmlFor=" ConfirmePassword ">  Confirme - Password :
+                   <input value={this.state.ConfirmePassword} name="ConfirmePassword" type="Password" id="ConfirmePassword" required="required" onChange={this.Hendelchange} /> <br />
+                                    </label><br />
+                                    {/* <textarea value={this.state.FamilyDescriptionvlue} name="FamilyDescription" id="Family-Description" name="Family_Description" placeholder="About the family.." onChange={this.Hendelchange}></textarea><br /> */}
+                                    <button required="required" type="submit"  > Submit </button>
+                                    {/* <div class="row">
+                                        <div class="col-50">
+                                            <label for="expyear">Exp Year</label>
+                                            <input type="text" id="expyear" name="expyear" placeholder="2018" />
+                                        </div>
+                                        <div class="col-50">
+                                            <label for="cvv">CVV</label>
+                                            <input type="text" id="cvv" name="cvv" placeholder="352" />
+                                        </div>
+                                    </div> */}
+                                </div>
+                            </div>
+                        </form>
 
-                    <h1>Family Form</h1>
-                    <label htmlFor="fname"> Family-Name :
-                    <input value={this.state.fname} name="fname"  type="text" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
+                    </div>
 
-                    <label htmlFor="email">  e-mail  :
-                    <input value={this.state.femail} name="email" id="email" type="email" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-                    <label htmlFor="Phone-Number">  Phone-Number  :
-                    <input value={this.state.fPhonNumber} name="fPhonNumber" id="Phone-Number" type="number" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-                    <label htmlFor="City"> City :
-                    <input value={this.state.fCity} name="fCity" id="City" type="text" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-                    <label htmlFor="NumberSoldiers">  Number of soliders you can host :
-                    <input value={this.state.fNumberSoldiersHosts} name="fNumberSoldiersHosts" id="NumberSoldiers" type="number" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-                    <label htmlFor="Password">   Password :
-                    <input value={this.state.fPassword} name="fPassword" type="Password" id="Password" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-                    <label htmlFor=" ConfirmePassword ">  Confirme - Password :
-                    <input value={this.state.ConfirmePassword} name="ConfirmePassword" type="Password" id="ConfirmePassword" required="required" onChange={this.Hendelchange} /> <br />
-                    </label><br />
-
-
-                    <textarea value={this.state.FamilyDescriptionvlue} name="FamilyDescription" id="Family-Description" name="Family_Description" placeholder="About the family.." onChange={this.Hendelchange}></textarea><br />
-
-                    <button required="required" type="submit"  > Submit </button>
-
-                </form>
+                </div>
             </div>
         )
-
     }
 }
-
-
--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 console.log("app is loading ....");
-
 const express = require("express");
-const router = express.Router();
 const app = express();
-
 app.use(express.json());
-
 const helperServer = require('./helperServer');
+
 
 app.get("/api", (req, res) => {
   console.log("root is accessed");
@@ -14,23 +11,21 @@ app.get("/api", (req, res) => {
 });
 
 
-
 app.post("/soldier",(req,res) => {
-  let soldierDetails = req.body;
-  console.log(soldierDetails , '------post soldier');
-  console.log('got req post in server');
+  let newObj = req.body;
+  console.log(newObj , '------------post soldier')
+  console.log('got req post in server')
   helperServer.findSoldiers(req,res)
 });
+ 
 
-
-
-  
 app.post("/family",(req,res)=>{
   console.log("this is the family");
- console.log(res.data);
-  res.status(201).send(req.data)
+  console.log(req.body);
+  res.status(201).send(req.body)
 })
 
+ 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
