db8eaf134a32c2e5080c92fc652352ccbc764dbe
Shay-bicha <shaybicha2@gmail.com>
2020-02-05 21:37:03 +0200

    ensure sync

--- client/src/Display-Matching-Families/DisplayMatchingFamilies.js
+++ client/src/Display-Matching-Families/DisplayMatchingFamilies.js
@@ -3,10 +3,27 @@ import "./DisplayMatchingFamilies.css";
 import solimg from "../images/soldiers-10.jpg";
 import axios from "axios";
 export default class DisplayMatchingFamilies extends Component {
-  state = {successMatch : false}
-  displayContacxt(){
-   
+  state = {ssuccessMatch : false};
+
+  displayContacxt(familiesArray , componentDisplayFamilies){
+   if(this.state.ssuccessMatch){
+     console.log('successMatch');
+      return `${localStorage.name}   Levi family approved your application and hosted you on the date 1.1.2020`;
+   }
+    else if(familiesArray.length > 0){
+    console.log('componentDisplayFamilies' , this.state.ssuccessMatch);
+    
+      return componentDisplayFamilies   
+   }
+   else{
+    console.log('No result');
+      return <h3>No result</h3>
+   }
   }
+
+
+  
+
   render() {
     const families = this.props.resultMatch;
     const displayFimilies = families.map((obj, index) => {
@@ -42,7 +59,8 @@ export default class DisplayMatchingFamilies extends Component {
                 message:`${localStorage.name} wants to stay with you on the ${localStorage.fromDate} date.`})
               .then(response => {
                 console.log(response);
-                // this.setState({ssuccessMatch : true})
+
+                this.setState({ssuccessMatch : true})
               })
               .catch(err => {
                 console.log(err);
@@ -56,7 +74,8 @@ export default class DisplayMatchingFamilies extends Component {
       <div className="warpResultMAtch">
 
           {/* {displayFimilies} */}
-        {(families.length > 0) ? displayFimilies : <h3>No result</h3>}
+        {/* {(families.length > 0) ? displayFimilies : <h3>No result</h3>} */}
+        {this.displayContacxt(families ,displayFimilies)}
       </div>
     )
   }
