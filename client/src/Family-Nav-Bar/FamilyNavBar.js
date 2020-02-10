56e93c7a63510e30a9fbb45e2645d599959abf4d
Astuol1tsagaow <57455850+Astuol1tsagaow@users.noreply.github.com>
2020-02-10 10:27:19 +0200

    add Abput page and styls  to the nav bars

--- client/src/Family-Nav-Bar/FamilyNavBar.js
+++ client/src/Family-Nav-Bar/FamilyNavBar.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './Familynavbar.css'
-
+import "./Familynavbar.css";
 
 export default function FamilyNavBar(props) {
-    return (
-        <div className="Navbar NavBarAfterRegistration" >
-            <Navbar className="Family-nav-bar" navbar bg="dark"  navbar="true">
-            <Link to="/"><img src="https://img.icons8.com/color/48/000000/camo-cream.png(2 kB)
+  return (
+    <div className="Navbar NavBarAfterRegistration">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src="https://img.icons8.com/color/48/000000/camo-cream.png(2 kB)
 https://img.icons8.com/color/48/000000/camo-cream.png
-"/></Link>    <div className="x">
-                <Nav className="mr-auto">
-                    <Link className="family-nav-bar" onClick = {()=>localStorage.clear()} to="/">Logout</Link>
-                    <Link className="family-nav-bar" to="/HistoryFamily">History</Link>
-                    <Link className="family-nav-bar" to="/">Setting</Link>
-                    <Link className="family-nav-bar" to="/">About Family</Link>
-                </Nav>
-                </div>
-            </Navbar>
-
-        </div>
-
-
-
-    )
+"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Link
+              className="family-nav-bar"
+              onClick={() => localStorage.clear()}
+              to="/"
+            >
+              Logout
+            </Link>
+            <Link className="family-nav-bar" to="/HistoryFamily">
+              History
+            </Link>
+            <Link className="family-nav-bar" to="/">
+              Setting
+            </Link>
+            <Link className="family-nav-bar" to="/">
+              About Family
+            </Link>
+          </Nav>
+          <Nav></Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      ;
+    </div>
+  );
 }
--- client/src/HistoryFamily/HistoryFamily.js
+++ client/src/HistoryFamily/HistoryFamily.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import Table from 'react-bootstrap/Table'
 
 export default class HistoryFamily extends Component {
     render() {
         return (
-            <div>
-                <h1>History Family Page</h1>
-            </div>
+            <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
         )
     }
 }
