54020e768456b64b5d5657b5df67335516e619eb
Shay-bicha <shaybicha2@gmail.com>
2020-02-10 16:00:30 +0200

    ensure sync

--- client/src/History-Page/HistoryPage.js
+++ client/src/History-Page/HistoryPage.js
@@ -2,22 +2,9 @@ import React, { Component } from 'react';
 import axios from "axios";
 import HistoryFamily from "../HistoryFamily/HistoryFamily";
 export default class historyPage extends Component {
-    state = {result : ""}
-    componentDidMount(){
-        let user = localStorage.user;
-        let emailSearch = {email : localStorage.email};
-        let userHistory;
-        (user === "soldier") ? userHistory = "/historySoldier" : userHistory = "/historyFamily"
-        axios.post(`${userHistory}` , {emailSearch})
-            .then(function (response) {
-                console.log(response.data);
-                this.setState({result :response.data });
-            })
-            .catch(function (error) {
-                
-                console.log(error);
-  })
-    }
+   
+   
+    
     render() {
         let user = localStorage.user;
          
@@ -28,5 +15,25 @@ export default class historyPage extends Component {
                 {(user === "soldier") ? "" : <HistoryFamily FamilyHistory = {this.state.result}/>}
             </div>
         )
+    }
+
+    componentDidMount(){
+        let currentComponent = this;
+        let user = localStorage.user;
+        let emailSearch = {email : localStorage.email};
+        let userHistory;
+        (user === "soldier") ? userHistory = "/historySoldier" : userHistory = "/historyFamily"
+        axios.post(`${userHistory}` , {emailSearch})
+            .then(function (response) {
+                console.log(response.data);
+                let temp = [...response.data]
+                currentComponent.setState({result : temp });
+                // console.log(currentComponent.state.result)
+            })
+            .catch(function (error) {
+                
+                console.log(error);
+
+  })
     }
 }
