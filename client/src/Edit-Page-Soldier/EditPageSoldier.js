12e18e70ec54efcfb1e11598eeb5bff4c5a27056
Shay-bicha <shaybicha2@gmail.com>
2020-02-12 11:49:24 +0200

    style edit page

--- client/src/Edit-Page-Soldier/EditPageSoldier.js
+++ client/src/Edit-Page-Soldier/EditPageSoldier.js
@@ -8,67 +8,67 @@ export default class EditPageSoldier extends Component {
         <form className="formEdit">
           <div class="form-row">
             <div class="form-group col-md-6">
-              <label for="inputEmail4">Name</label>
+              <label for="inputName">Name</label>
               <input
                 type="text"
                 class="form-control"
-                id="inputEmail4"
+                id="inputName"
                 placeholder="Name"
               />
             </div>
             <div class="form-group col-md-6">
-              <label for="inputPassword4">Last name</label>
+              <label for="inputLastName">Last name</label>
               <input
                 type="text"
                 class="form-control"
-                id="inputPassword4"
+                id="inputLastName"
                 placeholder="Last name"
               />
             </div>
             <div class="form-group warpEmail">
-              <label for="inputAddress2">Email</label>
+              <label for="inputEmail">Email</label>
               <input
                 type="email"
                 class="form-control"
-                id="inputAddress2"
+                id="inputEmail"
                 placeholder="Email"
               />
             </div>
             <div class="form-group col-md-6">
-              <label for="inputEmail4">password</label>
-              <input
-                type="email"
-                class="form-control"
-                id="inputEmail4"
-                placeholder="Email"
-              />
-            </div>
-            <div class="form-group col-md-6">
-              <label for="inputPassword4">Confrim Password</label>
+              <label for="inputPassword">password</label>
               <input
                 type="password"
                 class="form-control"
-                id="inputPassword4"
+                id="inputPassword"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputConfrimPassword">Confrim Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="inputConfrimPassword"
                 placeholder="Password"
               />
             </div>
           </div>
           <div class="form-group">
-            <label for="inputAddress">Phone</label>
+            <label for="inputPhone">Phone</label>
             <input
-              type="text"
+              type="number"
               class="form-control"
-              id="inputAddress"
-              placeholder="1234 Main St"
+              id="inputPhone"
+              placeholder="Phone"
             />
           </div>
           <div class="form-group">
-            <label for="inputAddress2">quite</label>
+            <label for="inputquite">Quite</label>
             <input
               type="text"
               class="form-control"
-              id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
+              id="inputquite"
+              placeholder="Quite"
             />
           </div>
           <div class="form-row">
@@ -76,30 +76,33 @@ export default class EditPageSoldier extends Component {
               <label for="inputCity">City</label>
               <input type="text" class="form-control" id="inputCity" />
             </div>
-            <div class="form-group col-md-4">
-              <label for="inputState">State</label>
-              <select id="inputState" class="form-control">
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
-            <div class="form-group col-md-2">
-              <label for="inputZip">Zip</label>
-              <input type="text" class="form-control" id="inputZip" />
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck" />
+
+            <div className ="warpCheckBox">
+            <input class="form-check-input" type="checkbox" id="gridCheck" />
               <label class="form-check-label" for="gridCheck">
                 Lone soldier:
               </label>
+              </div>
+          </div>
+          <div class="form-group">
+            <div class="form-check">
+              
             </div>
           </div>
-          <button type="submit" class="btn btn-primary">
-            Sign in
+          <button 
+          type="submit" 
+          class="btn btn-primary"
+          id = "editButton"
+          >
+            Edit account
           </button>
         </form>
+        <button 
+        className = "btn btn-danger"
+        id = "deleteButton"
+        >
+            Delete accuont
+        </button>
       </div>
     );
   }
